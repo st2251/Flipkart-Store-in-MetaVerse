db2e20b09ce495c3a43012a4b01b8724eded70c2
Saurabh Tripathi <looneydreamers2251@gmail.com>
2022-07-28 02:01:33 +0530

    added launchEvent Screen

--- src/game.ts
+++ src/game.ts
@@ -37,6 +37,134 @@ addSocialLink()
 addVideoScreen()
 addWearable()
 
+//flooring starts
+const entity = new Entity('entity')
+engine.addEntity(entity)
+entity.setParent(_scene)
+const gltfShape = new GLTFShape("models/FloorBaseTiles_01/FloorBaseTiles_01.glb")
+gltfShape.withCollisions = true
+gltfShape.isPointerBlocker = true
+gltfShape.visible = true
+entity.addComponentOrReplace(gltfShape)
+const transform21 = new Transform({
+  position: new Vector3(8, 0, -8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity.addComponentOrReplace(transform21)
+
+const entity2 = new Entity('entity2')
+engine.addEntity(entity2)
+entity2.setParent(_scene)
+entity2.addComponentOrReplace(gltfShape)
+const transform22 = new Transform({
+  position: new Vector3(24, 0, 8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity2.addComponentOrReplace(transform22)
+
+const entity3 = new Entity('entity3')
+engine.addEntity(entity3)
+entity3.setParent(_scene)
+entity3.addComponentOrReplace(gltfShape)
+const transform23 = new Transform({
+  position: new Vector3(40, 0, 8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity3.addComponentOrReplace(transform23)
+
+const entity4 = new Entity('entity4')
+engine.addEntity(entity4)
+entity4.setParent(_scene)
+entity4.addComponentOrReplace(gltfShape)
+const transform24 = new Transform({
+  position: new Vector3(8, 0, 24),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity4.addComponentOrReplace(transform24)
+
+const entity5 = new Entity('entity5')
+engine.addEntity(entity5)
+entity5.setParent(_scene)
+entity5.addComponentOrReplace(gltfShape)
+const transform25 = new Transform({
+  position: new Vector3(8,0,40),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity5.addComponentOrReplace(transform25)
+
+const entity6 = new Entity('entity6')
+engine.addEntity(entity6)
+entity6.setParent(_scene)
+entity6.addComponentOrReplace(gltfShape)
+const transform26 = new Transform({
+  position: new Vector3(-8,0,40),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity6.addComponentOrReplace(transform26)
+
+const entity7 = new Entity('entity7')
+engine.addEntity(entity7)
+entity7.setParent(_scene)
+entity7.addComponentOrReplace(gltfShape)
+const transform27 = new Transform({
+  position: new Vector3(-8, 0, 8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity7.addComponentOrReplace(transform27)
+
+const entity8 = new Entity('entity8')
+engine.addEntity(entity8)
+entity8.setParent(_scene)
+entity8.addComponentOrReplace(gltfShape)
+const transform28 = new Transform({
+  position: new Vector3(24, 0, -8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity8.addComponentOrReplace(transform28)
+
+const entity9 = new Entity('entity9')
+engine.addEntity(entity9)
+entity9.setParent(_scene)
+entity9.addComponentOrReplace(gltfShape)
+const transform29 = new Transform({
+  position: new Vector3(40, 0, -8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity9.addComponentOrReplace(transform29)
+
+const entity10 = new Entity('entity10')
+engine.addEntity(entity10)
+entity10.setParent(_scene)
+entity10.addComponentOrReplace(gltfShape)
+const transform30 = new Transform({
+  position: new Vector3(-8, 0, -8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity10.addComponentOrReplace(transform30)
+
+const entity11 = new Entity('entity11')
+engine.addEntity(entity11)
+entity11.setParent(_scene)
+entity11.addComponentOrReplace(gltfShape)
+const transform31 = new Transform({
+  position: new Vector3(-8, 0, 24),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity11.addComponentOrReplace(transform31)
+
+// flooring ends
+
 // change the eventUUID to your event
 // check following links about how to setup POAP event
 // https://docs.decentraland.org/development-guide/poap-tokens/
@@ -295,5 +423,67 @@ script1.spawn(imageFromURL4, {"image":"https://bafybeiegpazlbhaqw7mrl4eqnahxip5b
 script1.spawn(imageFromURL5, {"image":"https://bafybeiegpazlbhaqw7mrl4eqnahxip5bqh4ppf6mm3d7zity23ujgvnjdi.ipfs.dweb.link/avtarSuitQR.png"}, createChannel(channelId, imageFromURL5, channelBus))
 script1.spawn(imageFromURL6, {"image":"https://bafybeiavauicvjx5gfd7jaijhovjgo6jk7gmszd7r73iog357ioqsdnplq.ipfs.dweb.link/1600858456_HulSrV_Flipkart.jpg"}, createChannel(channelId, imageFromURL6, channelBus))
 
+//Video Streaming Platform starts
 
+// Base
+const base = new Entity()
+base.addComponent(new GLTFShape('models/baseDarkWithCollider.glb'))
+base.addComponent(new Transform({ 
+  position:new Vector3(16,0,16),
+  scale: new Vector3(2, 1, 2)
+}))
+engine.addEntity(base)
+
+// Seating
+const seat = new Entity()
+seat.addComponent(new GLTFShape('models/seats.glb'))
+seat.addComponent(new Transform({ 
+  position: new Vector3(24, 0.05, 32),
+  rotation: Quaternion.Euler(0,90,0)  
+}))
+engine.addEntity(seat)
+
+const screenBody = new Entity()
+screenBody.addComponent(new GLTFShape('models/screen.glb'))
+screenBody.addComponent(new Transform({ 
+  position: new Vector3(32, 0.05, 32),
+  rotation: Quaternion.Euler(0,90,0) 
+}))
+engine.addEntity(screenBody)
+
+// Screen
+const screenTransform = new Entity()
+screenTransform.addComponent(
+  new Transform({ position: new Vector3(0, 6.15, 5) })
+)
+screenTransform.getComponent(Transform).rotate(Vector3.Right(), -15)
+screenTransform.setParent(screenBody)
+
+const screen = new Entity()
+screen.addComponent(new PlaneShape())
+screen.addComponent(new Transform({ scale: new Vector3(19.2, 10.8, 1) }))
+screen.getComponent(Transform).rotate(Vector3.Up(), 180)
+screen.setParent(screenTransform)
+
+screenTransform.getComponent(Transform).scale.setAll(0.625) // You can change the scale of the screen here...
+
+// Video stream link from Vimeo
+const videoClip = new VideoClip(
+  'https://player.vimeo.com/external/552481870.m3u8?s=c312c8533f97e808fccc92b0510b085c8122a875'
+)
+const videoTexture = new VideoTexture(videoClip)
+videoTexture.play()
+videoTexture.loop = true
+
+// Adjust screen material to increase the brightness and clarity
+const screenMaterial = new Material()
+screenMaterial.albedoTexture = videoTexture
+screenMaterial.emissiveTexture = videoTexture
+screenMaterial.emissiveColor = Color3.White()
+screenMaterial.emissiveIntensity = 0.6
+screenMaterial.roughness = 1.0
+screen.addComponent(screenMaterial)
+
+
+//Video Streaming Platform ends
 
